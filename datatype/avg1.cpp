e42f00c82ee7840bbf9a1a90012f9cfde04c2402
Anup Singh <anoopsingh145g@gmail.com>
2026-05-30 19:55:54 +0530

    add avg1.cpp

---
+++ datatype/avg1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int a , b, c, d,e;
+
+    cin >> a >> b >> c >> d >> e;
+
+cout << "The average of a, b, c, d and e is: " << (a + b + c + d + e) / 5 << endl;
+
+    return 0;
+}
